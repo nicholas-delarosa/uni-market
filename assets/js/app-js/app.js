5b39df236bcae522096e456fbc71d02fe3544f11
CoderKing498 <yerlenisarrieta@gmail.com>
2026-07-10 22:25:11 -0500

    Implementación del modo oscuro en la app del proyecto, pendiente logica del proceso del script de Javascript

--- assets/js/app-js/app.js
+++ assets/js/app-js/app.js
@@ -367,3 +367,26 @@
   if (savedUniId && universities.some(u => u.id === savedUniId)) {
     selectUniversity(savedUniId);
   }
+
+
+  /* ================ DARK MODE ============== */
+    const body = document.body;
+    const modeToggle = document.getElementById("modeToggle");
+
+    const savedMode = localStorage.getItem("mode");
+
+    if (savedMode === "dark") {
+        body.classList.add("dark-mode");
+    }
+
+   modeToggle.addEventListener("click", () => {
+    console.log("Botón presionado");
+
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("mode", "dark");
+    } else {
+        localStorage.setItem("mode", "light");
+    }
+});
